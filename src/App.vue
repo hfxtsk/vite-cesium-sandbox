a8f1ca9014a97f6ed9b112eefbf2864647aad265
xuxb <xuwoool@qq.com>
2023-07-10 16:15:35 +0800

    refactor: 使用hash方式异步加载组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <Viewer>
-    <Toolbar></Toolbar>
+    <component :is="currentComp" v-if="currentComp"></component>
   </Viewer>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref, defineAsyncComponent, DefineComponent } from 'vue';
 import Viewer from './components/Viewer.vue';
-import Toolbar from './components/Toolbar.vue';
+import ErrorComp from './components/ErrorComp.vue';
+
+const currentComp = ref<DefineComponent>();
+
+onMounted(() => {
+  console.log('compName', window.location.hash);
+  const compName = window.location.hash.substring(1);
+  if (compName) {
+    currentComp.value = defineAsyncComponent({
+      loader: () => import(`./components/${compName}.vue`),
+      errorComponent: ErrorComp
+    });
+  }
+});
 </script>
 <style scoped></style>
---
+++ src/components/ErrorComp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+setTimeout(() => {
+  console.log(`${window.location.hash.substring(1)}组件不存在`);
+}, 500);
+</script>
+
+<template></template>
+
+<style></style>
